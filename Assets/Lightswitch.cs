1b3e1cc564ccb83651d46a61523f62ff70a59567
Tyler Trombley <tyler.trombley.15@gmail.com>
2020-08-26 18:26:57 -0600

    Completed up to the start of Part 1.2

---
+++ Assets/Lightswitch.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lightswitch : MonoBehaviour {
+
+	public Light light;
+	public bool ToggleLight;
+
+	// Use this for initialization
+	void Start () 
+	{
+		light = GetComponent<Light>();
+		ToggleLight = false;
+		light.color = new Color(0, 0, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(Input.GetKeyDown("tab"))
+        {
+			if(!ToggleLight)
+            {
+				light.color = new Color(1, 1, 1);
+			}
+			else
+            {
+				light.color = new Color(0, 0, 0);
+            }
+			ToggleLight = !ToggleLight;
+        }
+
+	}
+}
---
+++ Assets/Orbit.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using UnityEngine;
+
+public class Orbit : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		this.transform.Rotate(new Vector3(0, 2, 0));	
+	}
+}
