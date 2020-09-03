d426c69d454b8fa2169f75480413b34df68d2256
AdyThomas <u1120386@utah.edu>
2020-09-02 19:18:16 -0600

    Hopefully the lights work

---
+++ CS5360_Assignment1_2/Assets/GameScript.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameScript : MonoBehaviour {
+
+    public List<GameObject> lights = new List<GameObject>();
+    public float timer = 0;
+    public int score = 0;
+
+    // Use this for initialization
+    void Start () {
+        
+	}
+
+    public void Flush()
+    {
+
+        foreach (GameObject l in lights)
+        {
+            l.SetActive(false);
+        }
+
+        int rand = Random.Range(0, 3);
+
+        lights[rand].SetActive(true);
+
+        timer = 0;
+
+        score++;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        timer += Time.deltaTime;
+        
+        if(timer >= 3.0)
+        {
+            foreach(GameObject l in lights)
+            {
+                l.SetActive(false);
+            }
+
+            int rand = Random.Range(0, 3);
+
+            lights[rand].SetActive(true);
+
+            timer = 0;
+        }
+	}
+}
---
+++ CS5360_Assignment1_2/Assets/LightScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightScript : MonoBehaviour {
+
+    public GameScript game;
+
+	// Use this for initialization
+	void Start () {
+        game = GetComponentInParent<GameScript>();
+	}
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.gameObject.tag == "Player" && Input.GetButton("0"))
+        {
+            game.Flush();
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
