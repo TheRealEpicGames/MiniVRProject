cdef176f462a542ccfc11af30d7e5a595ca86fd9
AvantAveGarde <coltonlee4407@hotmail.com>
2020-09-09 17:56:21 -0600

    Part 3 completion

---
+++ CS5360_Assignment1_3/Assets/CameraScripts.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+public class CameraScripts : MonoBehaviour {
+    public VRMirror mirror;
+    public bool positionTracking = true;
+    public bool rotationTracking = true;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Tab)){
+            this.gameObject.transform.localPosition = new Vector3(0, 0, 0);
+            this.gameObject.transform.localRotation = Quaternion.identity;
+        }
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            this.gameObject.transform.localPosition = new Vector3(0, 0, 0);
+            this.gameObject.transform.Rotate(0, 180, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            mirror.ToggleMirror();
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            rotationTracking = !rotationTracking;
+        }
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            positionTracking = !positionTracking;
+        }
+
+        if (!positionTracking)
+        {
+            transform.position = -InputTracking.GetLocalPosition(XRNode.CenterEye);
+        }
+        if (!rotationTracking)
+        {
+            transform.rotation = Quaternion.Inverse(InputTracking.GetLocalRotation(XRNode.CenterEye));
+        }
+    }
+}
---
+++ CS5360_Assignment1_3/Assets/VRMirror.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRMirror : MonoBehaviour {
+    public bool isMirroring;
+    public GameObject player;
+    public Vector3 initialPlayerPos;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (isMirroring)
+        {
+            this.transform.localPosition = new Vector3(player.transform.localPosition[0], player.transform.localPosition[1], -player.transform.localPosition[2]) - initialPlayerPos;
+            this.transform.localRotation = Quaternion.Inverse(player.transform.localRotation);
+        }
+	}
+    
+    public void ToggleMirror()
+    {
+        if (isMirroring == false)
+        {
+            //initialPlayerPos = player.transform.localPosition;
+            initialPlayerPos = new Vector3(0, 0, 0);
+        }
+        isMirroring = !isMirroring;
+    }
+}
